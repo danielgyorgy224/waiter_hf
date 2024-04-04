2da3a0ff577c923babc005b832d7e2e07e1c438b
Norbiazseni <Bencecsanadi3@gmail.com>
2024-04-04 17:25:37 +0200

    Update waiter.py

--- waiter.py
+++ waiter.py
@@ -16,13 +16,34 @@ def összegzés2(o):
     for szam in o:
         sum+=round(szam%1,2)
     return sum
-        
+
+def max(o):
+    max_ertek = 0
+    for i in range(1, len(o)):
+        for j in range(1, len(o)):
+            if (o[i] > o[j]):
+                max_ertek = o[i]
+    return max_ertek
+
+def hanyadik(o):
+    sorszam = -1
+    i=0
+    while not o[i]==9:
+        i+=1
+    sorszam=i
+    return sorszam
+    
 
 
-def kiir(o,o_b,p):
+def kiir(o,o_b,p,m,h):
     print(f"A bevételek:{o}")
     print(f"Az óra bevétele {o_b}")
     print(f"Ennyi fontot keresett peniben kapott az órában:{p}")
+    print(f"A legnagyobb összeg amit fizettek neki: {m}")
+    print(f"{h}.ik vendég fizetett 9-et.")
+
+
+
 #főprogram
 #input
 osszegek = beolvas()
@@ -30,6 +51,8 @@ osszegek = beolvas()
 #számítás
 Az_ora_bevetele=összegzés(osszegek)
 peni=összegzés2(osszegek)
+max_kivalasztas = max(osszegek)
+hanyadik_vendeg = hanyadik(osszegek)
 
 #output
-kiir(osszegek,Az_ora_bevetele,peni)
+kiir(osszegek,Az_ora_bevetele,peni, max_kivalasztas, hanyadik_vendeg)
